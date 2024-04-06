c3ab61695efb7c573f2dd6bc1e05e602ead375e3
Hritvik-Mohan <hritvik5005@gmail.com>
2024-04-07 03:00:26 +0530

    Create a temp dashboard route for testing a private route

---
+++ src/routes/dashboardRoute.js
@@ -0,0 +1,18 @@
+const router = require("express").Router();
+const verify = require("./verifyToken");
+const User = require("../models/user");
+
+router.get("/", verify, async (req, res) => {
+  try {
+    const user = await User.findById(req.user);
+    console.log(user);
+    res.json({
+      greet: `Hello ${user.username}!`,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+module.exports = router;
