508da5b5f3c47e3f2a3e81b7566d53c066dead22
Hritvik-Mohan <hritvik5005@gmail.com>
2024-04-07 02:59:04 +0530

    Create register and login route

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,26 +1,68 @@
-const express = require('express');
+require('dotenv').config()
+const express = require("express");
 const router = express.Router();
-const User = require('../models/user');
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+const {
+  registerValidation,
+  loginValidation,
+} = require("../validation/validation");
+const bcrypt = require('bcryptjs');
 
-router.post('/register', async (req, res) => {
-    try {
-        const { username, email, password } = req.body;
+router.post("/register", async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
 
-        // Check if the user already exists
-        const existingUser = await User.findOne({ email });
-        if (existingUser) {
-            return res.status(400).json({ messsage: 'User already exists' });
-        }
+    // Validate the data before the user get's registers
+    const { error } = registerValidation({ username, email, password });
+    if (error) return res.status(400).send(error.details[0].message);
 
-        // Create a new user
-        const newUser = new User({ username, email, password });
-        await newUser.save();
-
-        res.status(201).json({ message: 'User registered successfully'});
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ message: 'Internal server error' });
+    // Check if the user already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
     }
+
+    // Hashing
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Create a new user
+    const newUser = new User({ username: username, email: email, password: hashedPassword });
+    await newUser.save();
+
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Validate the data before the user get's registers
+    const { error } = loginValidation({ email, password });
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ message: "Email or password is wrong!" });
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      res.status(401).json({ error: 'Invalid credentials' });
+    }
+
+    // Create and assign a token
+    const token = jwt.sign({_id: user._id}, process.env.TOKEN_SECRET);
+    res.header('auth-token', token).status(200).json({token, message: 'Login successful'});
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 });
 
 module.exports = router;
