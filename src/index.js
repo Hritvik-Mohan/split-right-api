cc82c9b358d8e678e2d3a27efcaa8faa74f1c95e
Hritvik-Mohan <hritvik5005@gmail.com>
2024-04-07 03:04:37 +0530

    Update index.js with user & dashboard route & a function to connect the db

--- src/index.js
+++ src/index.js
@@ -2,21 +2,25 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const connectDB = require("./config/database");
 const userRoutes = require("./routes/userRoutes");
+const dashboardRoute = require("./routes/dashboardRoute");
+
 const cors = require("cors");
+require('dotenv').config()
 
 // Connect to database
 connectDB();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 
 // Middleware
 app.use(bodyParser.json());
 
-// Routes
+// Routes Middleware
 app.use("/api/user", userRoutes);
+app.use("/api/dashboard", dashboardRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
